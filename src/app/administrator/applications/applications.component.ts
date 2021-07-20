7507960774efa9666a476470e17bd6147c56eab5
Kiran <kiran.varaprasad@gmail.com>
2021-07-21 01:58:41 +0530

    status change done

--- src/app/administrator/applications/applications.component.ts
+++ src/app/administrator/applications/applications.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/shared/services/api.service';
-import {MatSelectModule} from '@angular/material/select';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 
@@ -16,8 +16,14 @@ export class ApplicationsComponent implements OnInit {
   username: any;
   applyData: any;
   modalRef: BsModalRef;
-  editData: Object;
+  editData: any;
+  applyFormData: any = {}
   editApplyStatus: FormGroup
+  applicationData: FormData;
+  statusUpdated: any;
+  config = {
+    ignoreBackdropClick: true
+  };
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -25,25 +31,48 @@ export class ApplicationsComponent implements OnInit {
     private modalService: BsModalService,
     private fb: FormBuilder
   ) {
+    this.applicationData = new FormData();
     this.editApplyStatus = this.fb.group({
-      status: new FormControl([])
+      status: new FormControl('', [Validators.required])
     })
   }
-  editStatus(id, template: TemplateRef<any>){
+  editStatus(id, template: TemplateRef<any>) {
     this.apiService.getAppliesService(id)
-    .subscribe(
-      data => {
-        this.editData = data;
-        console.log(this.editData)
-      }
-    )
-    this.modalRef = this.modalService.show(template);
+      .subscribe(
+        data => {
+          this.editData = data;
+          console.log(this.editData)
+        }
+      )
+    this.modalRef = this.modalService.show(template, this.config);
   }
-  onChangeSelect(event){
+  onChangeSelect(event) {
     console.log(event.value)
+    this.statusUpdated = event.value;
   }
-  onChangeStatus(){
-    const fd = FormData;
+  onChangeStatus() {
+    this.applyFormData.orderId = this.editData.orderId;
+    this.applyFormData.rtiDetailsId = this.editData.rtiDetailsId;
+    this.applyFormData.applicationId = this.editData.applicationId;
+    this.applyFormData.personalDetailsId = this.editData.personalDetailsId;
+    this.applyFormData.serviceType = this.editData.serviceType;
+    this.applyFormData.status = this.statusUpdated;
+    this.applyFormData.selectedPlan = this.editData.selectedPlan;
+    this.applicationData.append('data', JSON.stringify(this.applyFormData));
+    this.apiService.putApplyService(this.editData.id, this.applicationData)
+      .subscribe(
+        data => {
+          console.log('modified data ' + data)
+          this.modalService.hide();
+          this.apiService.getApplyService()
+            .subscribe(
+              data => {
+                this.applyData = data;
+                this.editApplyStatus.reset();
+              }
+            );
+        }
+      )
   }
   ngOnInit(): void {
     const userData = JSON.parse(localStorage.getItem('user'))
@@ -52,7 +81,7 @@ export class ApplicationsComponent implements OnInit {
     this.apiService.getApplyService()
       .subscribe(
         data => {
-          console.log(data)
+          // console.log(data)
           this.applyData = data;
         }
       );
--- src/app/core/pipes/split-hyphen.pipe.ts
+++ src/app/core/pipes/split-hyphen.pipe.ts
@@ -7,7 +7,7 @@ export class SplitHyphenPipe implements PipeTransform {
 
   transform(value: string, ...args: unknown[]): unknown {
     const formatValue = value.split('-').join(' ');
-    console.log(formatValue)
+    // console.log(formatValue)
     const words = [];
     for (let word of formatValue.split(' ')) {
       words.push(word[0].toUpperCase() + word.slice(1));
