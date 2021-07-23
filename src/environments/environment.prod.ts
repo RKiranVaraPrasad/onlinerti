ca74fae8c6725b6533f313a4c8cca8a5aba90d99
Kiran <kiran.varaprasad@gmail.com>
2021-07-23 16:36:24 +0530

    env file is updated with https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://onlinerti.co:1337'
+  baseUrl: 'https://onlinerti.co:1337'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://onlinerti.co:1337'
+  baseUrl: 'https://onlinerti.co:1337'
 };
 
 /*
